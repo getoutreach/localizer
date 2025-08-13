babf1524995d541f3890c0a06079c7fb8df2b725
Mark Lee <mark.lee@outreach.io>
2025-08-12 18:59:42 -0700

    chore: replace copyright lines

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Outreach Corporation. All Rights Reserved.
+// Copyright 2022 Outreach Corporation. Licensed under the Apache License 2.0.
 // Copyright 2020 Jared Allard
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
